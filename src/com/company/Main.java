023e748fc49548a47e8c0fe498e5e257df15b123
Jaffar Abbas <gamaportal8@gmail.com>
2020-06-03 18:11:22 +0500

    push to laptop (3-6-2020    6:11 PM)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -58,6 +58,7 @@ class Current_Account extends Account_holder{
     double Minimum_balance;
     double Service_charges;
 
+
     public Current_Account(double Balance_Of_Account,double Minimum_balance,double Service_charges){
         super("jafafr","cur");
         this.Balance_of_Account = Balance_Of_Account;
@@ -66,24 +67,49 @@ class Current_Account extends Account_holder{
     }
 
     public String toString(){
-        return null;
+        return "Account Holder Name : "+Account_Holder_Name+"\n"+"Account Type : "+Account_Type+"\nAccount Number : "+Account_Number;
     }
     //Method 1
-    void Simple_interest(double interest){
-
+    void Simple_interest(double interest,int time_per_year){
+        //A = P(1 + rt)
+         double Simple_Interest = this.Balance_of_Account*(1+interest*time_per_year);
+         System.out.println("Interset Rate : "+interest+"\n\nInterst per "+time_per_year+"is = "+Simple_Interest); 
     }
     //Method 2
-    void Deposit_Payment(){}
+    void Deposit_Payment(double Payment_for_Deposit){
+        double Payment_after_Desposit=Balance_of_Account+Payment_for_Deposit;
+        System.out.println("Your Balance For Deposit is = "+Payment_for_Deposit+"\n\nYou Current Balance is = "+Balance_of_Account+"\n\nYou New Balance is = "+Payment_after_Desposit);
+    }
     //Method 3
-    void Payment_With_Draw(){}
+    void Payment_With_Draw(double Payment_to_With_Draw){
+         double Payment_after_With_Draw = Balance_of_Account - Payment_to_With_Draw;
+         if(Payment_to_With_Draw > Balance_of_Account){
+             System.out.println("You donot have enought Balance !!!!\n\nYour Wanted Balance for With draw is = "+Payment_to_With_Draw+"\n\nYour Current Balance is = "+Balance_of_Account);
+             System.out.println("You Have Alert !!!!!!!\n\n");
+             Check_for_Minimum_Balance(this.Minimum_balance,this.Service_charges);
+         }
+         else{
+             System.out.println("Your Old Balance is = "+Balance_of_Account+"\n\nYou With Draw = "+Payment_after_With_Draw+"\n\n\t\t\tHappy Banking!!!!!!!!");
+         }
+    }
+    //Method 4 (SubMethod)
+    void Check_for_Minimum_Balance(double Minimum_balance_check_for_method,double Service_charges_check_for_method){
+         if(Balance_of_Account < Minimum_balance_check_for_method){
+            System.out.println("Your Balance is ower Then The Appropiate Balance\n\n (Minimum balance = "+Minimum_balance_check_for_method+")\n\n konw You Have to Pay The Service Charge = "+Service_charges_check_for_method);
+         }
+         else{
+             System.out.println("Your Balance is Following the Rule is under Minimum Balance");
+         }
+    }
 }
 //Third Class (Derived class 2 and derived from first class account holder)
-class Saving_Account{}
+// class Saving_Account{}
 
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Current_Account obj = new Current_Account(10000,200,500);
+        System.out.println(obj.toString());
     }
 }
